19503645b745dd91238e7cec3e97e7374a9dbf33
rishabhchoudhary140126-eng <rishabhchoudhary140126@gmail.com>
2026-05-14 12:30:11 +0530

    added img

--- script.js
+++ script.js
@@ -32,6 +32,34 @@ cells.forEach(function(cell){
         board[row][col].count++;
         console.log(row, col, board[row][col].count);
 
+        if(board[row][col].count==0){
+            //no img
+            cell.innerHTML = "";
+        }
+        else if(board[row][col].count==1){
+            //1 ball img
+            cell.innerHTML = `<img src="imagegs/one-blue-ball.png" class="ball">`;
+        }
+        else if(board[row][col].count==2){
+            //2 ball img
+            cell.innerHTML = `<img src="imagegs/two-blue-ball.png" class="ball">`;
+        }
+        else if(board[row][col].count==3){
+            //3 ball img
+            cell.innerHTML = `<img src="imagegs/three-blue-ball.png" class="ball">`;
+        }
+        else if(board[row][col].count==4){
+            //explosion and reset count to 0
+            cell.innerHTML = `<img src="imagegs/explosion.gif" class="ball">`;
+            setTimeout(function(){
+
+                board[row][col].count = 0;
+
+                cell.innerHTML = "";
+
+            }, 600);
+        }
+
     });
 
 });
